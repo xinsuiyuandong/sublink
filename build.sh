7989454aba298a4901c0af2eb5f190ea530a8603
心隨緣動 <xeefei@gmail.com>
2025-08-29 14:31:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o sublink_amd64 main.go
-GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o sublink_arm64 main.go
-GOOS=windows  GOARCH=amd64  go build -ldflags="-w -s" -o sublink.exe main.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o sublink_linux_amd64 main.go
+GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o sublink_linux_arm64 main.go
+GOOS=windows  GOARCH=amd64  go build -ldflags="-w -s" -o sublink_windows_amd64.exe main.go
