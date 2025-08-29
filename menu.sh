259d2e897c06a28db58975834ca744217c5b18ed
心隨緣動 <xeefei@gmail.com>
2025-08-29 18:23:24 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
 function Up {
     # 获取最新的发行版标签
     latest_release=$(curl --silent "https://api.github.com/repos/xeefei/sublink/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
