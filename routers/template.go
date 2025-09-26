b2adaa189c2fe09bef23596485b90063cd22b533
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-27 02:00:02 +0800

    Update template.go

--- routers/template.go
+++ routers/template.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Templates(r gin.IRoutes) {
+func Templates(r *gin.IRoutes) {
 	TempsGroup := r.Group("/api/v1/template")
 	{
 		TempsGroup.POST("/add", api.AddTemp)
