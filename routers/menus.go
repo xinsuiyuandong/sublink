8ea5bc7c3ace2861e7fa302e1fa83fb057736509
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-27 02:06:17 +0800

    Update menus.go

--- routers/menus.go
+++ routers/menus.go
@@ -6,8 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Mentus(r *gin.IRoutes) {
-	MentusGroup := r.Group("/api/v1/menus")
+func Mentus(r gin.IRoutes) {
+	MentusGroup := r.privateGroup("/api/v1/menus")
 	{
 		// MentusGroup.GET("/menus", api.GetMenus)
 		MentusGroup.GET("/routes", api.GetMenus)
