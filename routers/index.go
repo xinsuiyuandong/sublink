8775da3d7641937ebf12f77fb35da29bd98d53fb
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-27 20:34:39 +0800

    Update index.go

--- routers/index.go
+++ routers/index.go
@@ -2,7 +2,7 @@ package routers
 
 // // 加载index.html
 // func Index(r *gin.Engine) {
-// 	TotalGroup := r.Group("/total")
+// 	TotalGroup := r.Group("/api/v1/total")
 // 	{
 // 		TotalGroup.GET("/sub", api.SubTotal)
 // 		TotalGroup.GET("/node", api.NodesTotal)
