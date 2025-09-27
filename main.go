067c3f8599db462f59da6433ab8892386fb39e3b
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-27 21:46:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 	var port = config.Port        // 读取端口号
 	// 获取版本号
 	var Isversion bool
-	version = "v2.1"
+	version = "v2.2"
 	flag.BoolVar(&Isversion, "version", false, "显示版本号")
 	flag.Parse()
 	if Isversion {
