2f3999373a6f8ab2c226589e6fc6f72ba3a9ddad
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-27 17:44:55 +0800

    Update total.go

--- routers/total.go
+++ routers/total.go
@@ -6,8 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Total(r *gin.RouterGroup) {
-	TotalGroup := r.Group("/total")
+func Total(r *gin.Engine) {
+	TotalGroup := r.Group("/api/v1/total")
 	{
 		TotalGroup.GET("/sub", api.SubTotal)
 		TotalGroup.GET("/node", api.NodesTotal)
