0ee9fdb7149e9759fda1faa5cabd5f86dbaee737
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-29 01:48:53 +0800

    Update config.go

--- models/config.go
+++ models/config.go
@@ -17,12 +17,12 @@ import (
 
 // Config 配置结构体
 type Config struct {
-	JwtSecret  string `yaml:"jwt_secret"`  // JWT密钥
+//	JwtSecret  string `yaml:"jwt_secret"`  // JWT密钥
 	ExpireDays int    `yaml:"expire_days"` // 过期天数
 	Port       int    `yaml:"port"`        // 端口号
 }
 
-var comment string = `# jwt_secret: JWT密钥
+// var comment string = `# jwt_secret: JWT密钥
 # expire_days: token 过期天数
 # port: 启动端口
 `
@@ -31,10 +31,10 @@ var comment string = `# jwt_secret: JWT密钥
 func ConfigInit() {
 	// 检查配置文件是否存在
 	if _, err := os.Stat("./db/config.yaml"); os.IsNotExist(err) {
-		R := utils.RandString(31) // 生成随机字符串作为JWT密钥
+	//	R := utils.RandString(31) // 生成随机字符串作为JWT密钥
 		// 如果不存在则创建默认配置文件
 		defaultConfig := Config{
-			JwtSecret:  R, // 生成随机JWT密钥
+	//		JwtSecret:  R, // 生成随机JWT密钥
 			ExpireDays: 14,
 			Port:       8000, // 默认端口
 		}
@@ -70,9 +70,9 @@ func ReadConfig() Config {
 func SetConfig(newCfg Config) {
 	oldCfg := ReadConfig() // 读取旧的配置文件
 	// 覆盖新的字段
-	if newCfg.JwtSecret != "" {
-		oldCfg.JwtSecret = newCfg.JwtSecret
-	}
+//	if newCfg.JwtSecret != "" {
+//		oldCfg.JwtSecret = newCfg.JwtSecret
+//	}
 	if newCfg.ExpireDays != 0 {
 		oldCfg.ExpireDays = newCfg.ExpireDays
 	}
