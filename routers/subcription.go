1025a80174f270f6ae4934700a12023f01baf9c1
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-29 01:19:25 +0800

    Update subcription.go

--- routers/subcription.go
+++ routers/subcription.go
@@ -18,22 +18,4 @@ func Subcription(r *gin.Engine) {
 		SubcriptionGroup.GET("/get", api.SubGet)
 		SubcriptionGroup.POST("/update", api.SubUpdate)
 	}
-
-	// --- 【核心新增】: 处理短链接访问的公开路由 ---
-	// 〔中文注释〕: 当手机 App 访问例如 /c/FjK8sLp3 时，这个路由会捕获请求
-	r.GET("/c/:code", func(c *gin.Context) {
-		// 1. 从 URL 路径中获取随机码
-		code := c.Param("code")
-		var sub models.Subcription
-
-		// 2. 根据这个随机码去数据库的 'code' 字段中查找对应的记录
-		if err := models.DB.Where("code = ?", code).First(&sub).Error; err != nil {
-			log.Printf("短链接解析失败: 未找到代码 '%s', 错误: %v", code, err)
-			c.String(http.StatusNotFound, "Link Not Found") // 〔中文注释〕: 如果找不到，返回 404
-			return
-		}
-
-		// 3. 【重要】: 如果找到了记录，就将存储在 Config 字段中的原始长链接 (vless://...) 作为响应返回
-		c.String(http.StatusOK, sub.Config)
-	})
 }
