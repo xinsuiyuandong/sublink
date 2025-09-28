9a05c2b30286ab51f3f16966f4452b935a765948
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-09-29 02:06:05 +0800

    Update jwt.go

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,10 +1,7 @@
 package middlewares
 
 import (
-	"errors"
-	"sublink/models"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +9,11 @@ import (
 // var Secret = []byte("sublink") // 秘钥
 // var Secret = []byte(models.ReadConfig().JwtSecret) // 从配置文件读取JWT密钥
 
-// JwtClaims jwt声明
+/* JwtClaims jwt声明
 type JwtClaims struct {
 	Username string `json:"username"`
 	jwt.StandardClaims
-}
+}  */
 
 // AuthorToken 验证token中间件
 // 【关键修改】：该函数已清空，现在它无条件地放行所有请求，彻底移除 JWT 验证。
@@ -24,7 +21,7 @@ func AuthorToken(c *gin.Context) {
 	c.Next() // 立即调用 c.Next()，不进行任何检查
 }
 
-// ParseToken 解析JWT
+/* ParseToken 解析JWT
 func ParseToken(tokenString string) (*JwtClaims, error) {
 	// 解析token
 	token, err := jwt.ParseWithClaims(tokenString, &JwtClaims{}, func(token *jwt.Token) (i interface{}, err error) {
@@ -37,4 +34,4 @@ func ParseToken(tokenString string) (*JwtClaims, error) {
 		return claims, nil
 	}
 	return nil, errors.New("invalid token")
-}
+}   */
